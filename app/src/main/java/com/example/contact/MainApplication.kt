bde1e1611e63b57654a29ef9d73107aaddf487c2
RodoBarriosG <ext_rodbarri@mercadolibre.com.co>
2021-08-27 17:11:45 -0500

    add initial workManager config

--- app/src/main/java/com/example/contact/MainApplication.kt
+++ app/src/main/java/com/example/contact/MainApplication.kt
@@ -1,8 +1,15 @@
 package com.example.contact
 
 import android.app.Application
+import androidx.work.Configuration
 import com.example.contact.database.ContactDatabase
 
-class MainApplication: Application() {
+class MainApplication : Application(), Configuration.Provider {
+
     val database: ContactDatabase by lazy { ContactDatabase.getDatabase(this) }
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setMinimumLoggingLevel(android.util.Log.DEBUG)
+            .build()
 }
