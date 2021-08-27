9ca0bb6fc6d5fadb79b3f2555495325b2f18a6f6
RodoBarriosG <ext_rodbarri@mercadolibre.com.co>
2021-08-27 16:52:39 -0500

    add new useCase for insert contact

--- app/src/main/java/com/example/contact/viewmodels/ContactViewModel.kt
+++ app/src/main/java/com/example/contact/viewmodels/ContactViewModel.kt
@@ -3,14 +3,27 @@ package com.example.contact.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.contact.database.entity.Contact
 import com.example.contact.usecases.GetAllContactUseCase
+import com.example.contact.usecases.SetContactUseCase
+import kotlinx.coroutines.launch
 
-class ContactViewModel(private val getAllContactUseCase: GetAllContactUseCase): ViewModel() {
+class ContactViewModel(
+    private val getAllContactUseCase: GetAllContactUseCase,
+    private val setContactUseCase: SetContactUseCase
+): ViewModel() {
 
     private val contactEventMutableLiveData = MutableLiveData<ContactEvent>()
     val contactEventLiveData: LiveData<ContactEvent> get() = contactEventMutableLiveData
 
     fun getAll() {
-        contactEventMutableLiveData.value = ContactEvent.setContactList(getAllContactUseCase.invoke())
+        contactEventMutableLiveData.value = ContactEvent.SetContactList(getAllContactUseCase.invoke())
+    }
+
+    fun insert(contact: Contact) {
+        viewModelScope.launch {
+            setContactUseCase.invoke(contact)
+        }
     }
 }
--- app/src/main/java/com/example/contact/viewmodels/ContactViewModelFactory.kt
+++ app/src/main/java/com/example/contact/viewmodels/ContactViewModelFactory.kt
@@ -2,13 +2,17 @@ package com.example.contact.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.contact.database.ContactDao
+import com.example.contact.database.dao.ContactDao
 import com.example.contact.repository.ContactRepositoryImpl
 import com.example.contact.usecases.GetAllContactUseCase
+import com.example.contact.usecases.SetContactUseCase
 
 class ContactViewModelFactory(private val contactDao: ContactDao): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return ContactViewModel(GetAllContactUseCase(ContactRepositoryImpl(contactDao))) as T
+        return ContactViewModel(
+            GetAllContactUseCase(ContactRepositoryImpl(contactDao)),
+            SetContactUseCase(ContactRepositoryImpl(contactDao))
+        ) as T
     }
 }
