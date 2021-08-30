d5434fc8ab03fa3e5838c93d867275554be2069e
RodoBarriosG <ext_rodbarri@mercadolibre.com.co>
2021-08-30 10:41:10 -0500

    unused code is removed

--- app/src/main/java/com/example/contact/viewmodels/ContactViewModel.kt
+++ app/src/main/java/com/example/contact/viewmodels/ContactViewModel.kt
@@ -3,15 +3,10 @@ package com.example.contact.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.contact.database.entity.Contact
 import com.example.contact.usecases.GetAllContactUseCase
-import com.example.contact.usecases.SetContactUseCase
-import kotlinx.coroutines.launch
 
 class ContactViewModel(
-    private val getAllContactUseCase: GetAllContactUseCase,
-    private val setContactUseCase: SetContactUseCase
+    private val getAllContactUseCase: GetAllContactUseCase
 ): ViewModel() {
 
     private val contactEventMutableLiveData = MutableLiveData<ContactEvent>()
@@ -19,11 +14,5 @@ class ContactViewModel(
 
     fun getAll() {
         contactEventMutableLiveData.value = ContactEvent.SetContactList(getAllContactUseCase.invoke())
-    }
-
-    fun insert(contact: Contact) {
-        viewModelScope.launch {
-            setContactUseCase.invoke(contact)
-        }
     }
 }
--- app/src/main/java/com/example/contact/viewmodels/ContactViewModelFactory.kt
+++ app/src/main/java/com/example/contact/viewmodels/ContactViewModelFactory.kt
@@ -5,14 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.contact.database.dao.ContactDao
 import com.example.contact.repository.ContactRepositoryImpl
 import com.example.contact.usecases.GetAllContactUseCase
-import com.example.contact.usecases.SetContactUseCase
 
 class ContactViewModelFactory(private val contactDao: ContactDao): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return ContactViewModel(
-            GetAllContactUseCase(ContactRepositoryImpl(contactDao)),
-            SetContactUseCase(ContactRepositoryImpl(contactDao))
+            GetAllContactUseCase(ContactRepositoryImpl(contactDao))
         ) as T
     }
 }
